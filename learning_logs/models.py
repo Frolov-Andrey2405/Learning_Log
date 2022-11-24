f90e4e839489f090be8a0fc27964a48d00d43a7c
ZaNoZa2405 <senior.slender2015@gmail.com>
2022-11-24 19:58:11 +0200

    Entry Model Definition and Migration

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Topic(models.Model):
     '''The topic the user is studying.'''
     text = models.CharField(max_length=200)
@@ -10,3 +11,19 @@ class Topic(models.Model):
     def __str__(self):
         '''Returns a string representation of the model'''
         return self.text
+
+
+class Entry(models.Model):
+    '''Information studied by the user on the topic.'''
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        '''The Meta class is embedded in the Entry class. 
+        The Meta class stores additional information about the model management'''
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        '''Returns the string representation of the model'''
+        return f'{self.text[:50]}...'
