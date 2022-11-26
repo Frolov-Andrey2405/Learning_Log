08b8e336095fc25065382d74ffe5b447f356a747
ZaNoZa2405 <senior.slender2015@gmail.com>
2022-11-26 12:43:57 +0200

    Writing a Submission

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    '''Learning Log Homepage'''
+    return render(request, 'Learning_logs/index.html', )
+    
