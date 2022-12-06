02bfd82e3c2529d13a700888abe6e8497c80c697
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2022-12-06 11:59:54 +0200

    URL for new_entry

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,6 +5,7 @@ from . import views  # The dot tells Python to import the views from the directo
 
 # The app_name variable helps Django to distinguish this urls.py file from files with the same name in other applications in the project
 app_name = 'learning_logs'
+
 urlpatterns = [
     # Homepage
     path('', views.index, name='index'),
@@ -17,5 +18,8 @@ urlpatterns = [
     path('topics/<int:topic_id>/', views.topic, name='topic'),
 
     # Page for adding a new topic
-    path('new_topic/', views.new_topic, name='new_topic')
+    path('new_topic/', views.new_topic, name='new_topic'),
+
+    # Table for adding a new entry
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
 ]
