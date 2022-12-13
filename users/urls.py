51a8c919dd8ba8efcfe0cf66ed805c92ed0f0df3
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2022-12-13 11:11:53 +0200

    Adding explanatory comments in the code

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,15 @@
-"""Определяет схемы URL для пользователей"""
+"""Defines URL schemes for users"""
+
 from django.urls import path, include
+# First the path function is imported and then the include function
+# to include the default authentication URLs defined by Django
 
 app_name = 'users'
+# The app_name variable is set to 'users' so that the Django infrastructure
+# can distinguish these URLs from URLs belonging to other applications
+
 urlpatterns = [
-    # Включить URL авторизации по умолчанию.
+    # Turn on the default authorization URL.
     path('', include('django.contrib.auth.urls')),
+    # http://localhost:8000/users/login/
 ]
