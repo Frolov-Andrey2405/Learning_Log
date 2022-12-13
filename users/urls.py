9de0f5e5fcd95ceada2912303683972ef3ea054f
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2022-12-13 11:33:49 +0200

    he register URL

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 # First the path function is imported and then the include function
 # to include the default authentication URLs defined by Django
 
+from . import views
+
 app_name = 'users'
 # The app_name variable is set to 'users' so that the Django infrastructure
 # can distinguish these URLs from URLs belonging to other applications
@@ -12,4 +14,7 @@ urlpatterns = [
     # Turn on the default authorization URL.
     path('', include('django.contrib.auth.urls')),
     # http://localhost:8000/users/login/
+
+    # Registration page
+    path('register/', views.register, name='register'),
 ]
