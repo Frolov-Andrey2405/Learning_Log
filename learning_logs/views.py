84c3211844754ad04c9e7680003c0f8405a957d9
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2022-12-14 16:29:29 +0200

    Limiting access to topics

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -10,14 +10,16 @@ def index(request):
     '''The home page for Learning Log'''
     return render(request, 'learning_logs/index.html')
 
+
 @login_required
 def topics(request):
     """Show all topics"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     # A query to the database for Topic objects sorted by the date_added attribute is issued
     # The context to be passed to the template is defined
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
 
 @login_required
 def topic(request, topic_id):
